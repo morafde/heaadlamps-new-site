8b7ebfbf6c29150732418b6346030642f5c7f997
mor <morafde@yahoo.com>
2024-06-06 20:01:44 +0100

    chnages

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Home ',
+  description: 'Headlamps Consulting Services',
 }
 
 import Hero from '@/components/hero'
