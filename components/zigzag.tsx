59fd538add009c358c8ef3df3ace776c9cc50fdb
morafde <27872464+morafde@users.noreply.github.com>
2025-03-18 10:20:02 +0100

    Update zigzag.tsx

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -139,7 +139,7 @@ export default function Zigzag() {
                       >
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
                       </svg>
-                      <span>Assessment</span>
+                      <span>AI-Powered Assessments and Remediation</span>
                     </li>
                     <li className="flex items-center mb-2">
                       <svg
